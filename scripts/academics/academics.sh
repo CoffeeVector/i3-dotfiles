bd3f69fcf5007e1b77341c3fe329082dfbca15d5
coffeevector <coffeevector@gmail.com>
2018-12-26 14:42:46 -0800

    Handles PascalCase and camelCase for academics

--- scripts/academics/academics.sh
+++ scripts/academics/academics.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-course=$(ls -l ~/Academics/UCMerced/Spring2019 | awk '{ if (NR !=1) {print $9}}' | sed "s/\([A-Z]\)/ \1/g;s/ //" | rofi -dmenu -i -p "Academics: ")
+course=$(ls -l ~/Academics/UCMerced/Spring2019 | awk '{ if (NR !=1) {print $9}}' | sed "s/\([A-Z]\)/ \1/g" | awk '{if (substr($0, 1, 1) ~ / /){print substr($0, 2)} else {print $0} }' | rofi -dmenu -i -p "Academics: ")
 course=$(echo -e $course | sed "s/ //g" )
 if [ "$course" != "" ]; then
 	st -e ranger ~/Academics/UCMerced/Spring2019/$course
