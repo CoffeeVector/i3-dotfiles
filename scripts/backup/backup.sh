21b02944b15f2d858eb5c3250b934e23f3281cb9
coffeevector <coffeevector@gmail.com>
2019-01-04 13:58:20 -0800

    Renamed backup

--- scripts/backup/backup.sh
+++ scripts/backup/backup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-case $(echo -e "Restic Backup\nRestic Forget\nRestic Prune\nDrive Push" | rofi -dmenu -i -p "Computer") in
+case $(echo -e "Restic Backup\nRestic Forget\nRestic Snapshots\nRestic Prune\nDrive Push" | rofi -dmenu -i -p "Restic Backup") in
 	Restic\ Backup)
 		/home/coffeevector/.config/i3/scripts/backup/backup-backup.sh
 		;;
 	Restic\ Forget)
 		/home/coffeevector/.config/i3/scripts/backup/backup-forget.sh
+		;;
+	Restic\ Snapshots)
+		/home/coffeevector/.config/i3/scripts/backup/backup-snapshots.sh
 		;;
 	Restic\ Prune)
 		/home/coffeevector/.config/i3/scripts/backup/backup-prune.sh
