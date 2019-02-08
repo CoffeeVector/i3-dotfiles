ef76a5dd22f59d9f6ab867f900f62698b8168638
coffeevector <coffeevector@gmail.com>
2019-02-08 09:48:25 -0800

    Added listlessness theme

--- scripts/theming/theming.sh
+++ scripts/theming/theming.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-case $(echo -e "Wood\nSakura\nPrimitive Sun" | rofi -dmenu -i -p "Themeing") in
+case $(echo -e "Wood\nSakura\nPrimitive Sun\nListlessness" | rofi -dmenu -i -p "Themeing") in
 	Wood)
 		rm ~/Pictures/Themes/Wallpaper.png
 		ln -s ~/Pictures/Themes/Wood/Wood.png ~/Pictures/Themes/Wallpaper.png
@@ -19,6 +19,13 @@ case $(echo -e "Wood\nSakura\nPrimitive Sun" | rofi -dmenu -i -p "Themeing") in
 		ln -s ~/Pictures/Themes/PrimitiveSun/PrimitiveSun.png ~/Pictures/Themes/Wallpaper.png
 		rm ~/Pictures/Themes/WallpaperOverlay.png
 		ln -s ~/Pictures/Themes/PrimitiveSun/PrimitiveSunOverlay.png ~/Pictures/Themes/WallpaperOverlay.png
+		feh --bg-fill '/home/coffeevector/Pictures/Themes/Wallpaper.png'
+		;;
+	Listlessness)
+		rm ~/Pictures/Themes/Wallpaper.png
+		ln -s ~/Pictures/Themes/Listlessness/listlessness.png ~/Pictures/Themes/Wallpaper.png
+		rm ~/Pictures/Themes/WallpaperOverlay.png
+		ln -s ~/Pictures/Themes/Listlessness/listlessnessOverlay.png ~/Pictures/Themes/WallpaperOverlay.png
 		feh --bg-fill '/home/coffeevector/Pictures/Themes/Wallpaper.png'
 		;;
 esac
